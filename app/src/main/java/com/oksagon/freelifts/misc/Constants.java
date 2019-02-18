9eed7b37b6c737ed54e455bdec86aec82210236b
Oksanen <E0474327@eaton.ad.etn.com>
2019-02-18 09:22:29 +0100

    Basic model stuff implemented

--- app/src/main/java/com/oksagon/freelifts/misc/Constants.java
+++ app/src/main/java/com/oksagon/freelifts/misc/Constants.java
@@ -1,4 +1,15 @@
 package com.oksagon.freelifts.misc;
 
+/**
+ * Constants
+ * @author Janne Oksanen <janne.oksanen@gmail.com>
+ * @since 2019-02-15
+ */
 public class Constants {
+
+    /**
+     * Enumerates different kinds of lifts that can be performed in trainig programs
+     */
+    public enum LiftType { SQUAT, BENCH_PRESS, DEADLIFT, ROW, OVERHEAD_PRESS }
+
 }
--- app/src/main/java/com/oksagon/freelifts/model/Workout.java
+++ app/src/main/java/com/oksagon/freelifts/model/Workout.java
@@ -1,4 +1,60 @@
 package com.oksagon.freelifts.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Models a single workout in the training cycle.
+ * @author Janne Oksanen <janne.oksanen@gmail.com>
+ * @since 2019-02-15
+ */
 public class Workout {
+
+    /**
+     * The sequence number of the {@link Workout} in the current {@link Program} cycle
+     */
+    final int mSequenceNumber;
+
+    /**
+     * Sets to be performed in this workout
+     */
+    List<Set> mSets;
+
+    /**
+     * Constructor
+     * @param sequenceNumber Sequence number within the training cycle
+     * @param sets {@link Set}s to be performed
+     */
+    public Workout(final int sequenceNumber, final List<Set> sets) {
+        this.mSequenceNumber = sequenceNumber;
+        this.mSets = sets;
+    }
+
+    /**
+     * Get a list of the sets
+     * @return List of sets or empty list
+     */
+    public List<Set> getSets(){
+        if ( mSets == null ){
+            mSets = new ArrayList<>();
+        }
+        return mSets;
+    }
+
+    /**
+     * Add a new set to the {@link Workout}
+     * @param set new set
+     * @param position where does it go in the workout
+     */
+    public void addSet( final Set set, final int position ){
+        mSets.add(position, set);
+    }
+
+    /**
+     * Remove set from the workout
+     * @param position
+     */
+    public void removeSet( final int position ) {
+        mSets.remove(position);
+    }
 }
